287d25faba3f184f87ae880eac99df1f603571f8
Jack Yeh <jack.yeh@gmail.com>
2015-05-14 09:51:20 -0600

    add password authentication

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { in: 6..50 }
+  validates :email, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  validates_format_of :email, {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
+end
