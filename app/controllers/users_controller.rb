a8daecbddbfa3bb63460e1c59a58ee56eb43bda5
Jack Yeh <jack.yeh@gmail.com>
2015-05-23 07:57:22 -0600

    pass test: unsucessufl edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,6 +19,14 @@ class UsersController < ApplicationController
       redirect_to  @user
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'edit'
     end
   end
 
