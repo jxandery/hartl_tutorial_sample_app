dee91f2703c33ff1be95f3c66fa9821b6068d012
Jack Yeh <jack.yeh@gmail.com>
2015-05-23 08:08:47 -0600

    pass test: successful edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   validates_format_of :email, {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
