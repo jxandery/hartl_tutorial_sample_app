3103d4e0463bcb626cc6858b99ba6da2a550cf51
Jack Yeh <jack.yeh@gmail.com>
2015-05-23 21:17:48 -0600

    pass test:successful edit with friendly forwarding

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -56,6 +56,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test 'login without remembering' do
     log_in_as(@user, remember_me: '0')
-    assert_not_nil cookies['remember_token']
+    assert_nil cookies['remember_token']
   end
 end
