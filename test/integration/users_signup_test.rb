92bec1ab7e67b16bac77c63d7d7509da68044ec8
Jack Yeh <jack.yeh@gmail.com>
2015-05-26 18:15:16 -0600

    pass test:should redirect index when not logged in

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: { name: "",
                                email: "users@invalid",
                                password: "foo",
-                               password_confirmation: "foobar"}
+                               password_confirmation: "bar"}
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div=alert alert-danger'
   end
 
   test 'valid signup information' do
@@ -20,6 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                            password_confirmation: "password"}
     end
     assert_template 'users/show'
-    assert is_logged_in?
+    assert_not flash.nil?
   end
 end
