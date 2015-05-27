2c18abb849af984e1cb0404ca183ccf912f8436b
Jack Yeh <jack.yeh@gmail.com>
2015-05-27 13:37:26 -0600

    pass test:user mailer of current email implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { in: 6..50 }
   validates :email, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
@@ -30,5 +31,16 @@ class User < ActiveRecord::Base
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
